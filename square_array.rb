19c8e00da0ddeb7f03584ddb02a229895c7357e7
Eli Huebner <elimhuebner@gmail.com>
2020-09-09 17:48:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  squared_array = []
-  array.each do |index|
-    squared_array.push(index**2)
-  end
-  squared_array
+  # squared_array = []
+  # array.each do |index|
+  #   squared_array.push(index**2)
+  # end
+  # squared_array
 end
